9f90a8ee78ae1cc4ca6e66b253b79f34e9be0bc3
Pedro Sousa <pedronsousa@gmail.com>
2020-01-08 08:24:21 +0000

    Project setup

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const f = () => "Hello"
+
+console.log(f())
---
+++ test/index.spec.ts
@@ -0,0 +1,7 @@
+import { f } from "../src"
+
+describe("test", () => {
+  it("1", () => {
+    expect(f()).toEqual("Hello")
+  })
+})
